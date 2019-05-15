f96c099fba6c9a6ab7c40f831772ce2bcd256bbb
magdalena-u <magdalena.urzon@gmail.com>
2019-05-15 20:47:31 +0200

    Iterable groups

---
+++ 6_3_iterable.js
@@ -0,0 +1,25 @@
+class Group {
+    constructor(arr) {
+        this.cluster = arr;
+    }
+    add = value => (this.cluster.includes(value) ? null : this.cluster.push(value));
+    delete = value =>
+        this.cluster.includes(value) ? this.cluster.splice(this.cluster.indexOf(value), 1) : null;
+    has = value => this.cluster.some(item => item === value);
+
+    static from(arr) {
+        return new Group(arr);
+    }
+
+    [Symbol.iterator]() {
+        let data = this.cluster
+        return {
+            next() {
+                return {
+                    done: data.length === 0,
+                    value: data.shift()
+                }
+            }
+        }
+    }
+}
