66be9a37b43119768a7ac35ff971a1b58c099b78
magdalena-u <magdalena.urzon@gmail.com>
2019-05-15 12:53:33 +0200

    implement reverse

--- reverse.js
+++ reverse.js
@@ -1,3 +1,13 @@
-const reverseArray = arr => (result = arr.map(i => i).reverse());
-
-const reverseArrayInPlace = arr => arr.reverse();
+const reverseArray = arr => {
+    let newArr = []
+    arr.map(i => newArr.unshift(i));
+    return newArr
+}
+const reverseArrayInPlace = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        let a = arr[i]
+        arr[i] = arr[arr.length - 1 - i]
+        arr[arr.length - 1 - i] = a
+        return arr
+    }
+}
