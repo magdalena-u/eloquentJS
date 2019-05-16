68930f2ed2e9ec09e3b8d6bc2ee1ad2be99c0f84
magdalena-u <magdalena.urzon@gmail.com>
2019-05-16 12:26:25 +0200

    code formatting

--- chessboard.js
+++ chessboard.js
@@ -1,16 +1,15 @@
 const chessboard = () => {
-    let board ="";
+    let board = "";
     let size = 8;
-    for (let i = 0; i<=size; i++) {
-        for (let index = 0; index<=size; index++){
-            if ((i+index)%2=== 0) {
+    for (let i = 0; i <= size; i++) {
+        for (let index = 0; index <= size; index++) {
+            if ((i + index) % 2 === 0) {
                 board += '#';
-            }
-            else board += ' ';
-            if( index === 8) {
+            } else board += ' ';
+            if (index === 8) {
                 board += "\n"
             }
         }
     }
-console.log(board);
+    console.log(board);
 }
