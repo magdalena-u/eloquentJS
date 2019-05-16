1346abb75e1a340b34acefa8334ca07598d3874f
magdalena-u <magdalena.urzon@gmail.com>
2019-05-16 12:23:37 +0200

    range helper instead of loop

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,11 @@
-const fizzbuzz = () => {
-    for (let i=1; i<=100; i++) {
-        if (i%3===0 && i%5===0) {
-            console.log("FizzBuzz")
-        }
-        else if (i%3===0){
-            console.log("Fizz")
-        }
-        else if (i%5 === 0) {
-            console.log("Buzz")
-        }
-        else console.log(i)
-    }
-}
+const range = n => new Array(n).fill().map((_, i) => i)
+
+const run = n => range(n).forEach(item => {
+    if (item % 3 === 0 && item % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (item % 5 === 0) {
+        console.log("Fizz")
+    } else if (item % 3 === 0) {
+        console.log("Buzz")
+    } else console.log(item)
+})
