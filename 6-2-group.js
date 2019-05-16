65a38321e82a2f5adaa659aff03ce6169e7a9532
magdalena-u <magdalena.urzon@gmail.com>
2019-05-16 16:00:34 +0200

    change ternary operator for logical operator

--- 6-2-group.js
+++ 6-2-group.js
@@ -2,9 +2,9 @@ class Group {
     constructor(arr) {
         this.cluster = arr;
     }
-    add = value => (this.cluster.includes(value) ? null : this.cluster.push(value));
+    add = value => this.cluster.includes(value) || this.cluster.push(value);
     delete = value =>
-        this.cluster.includes(value) ? this.cluster.splice(this.cluster.indexOf(value), 1) : null;
+        this.cluster.includes(value) && this.cluster.splice(this.cluster.indexOf(value), 1);
     has = value => this.cluster.some(item => item === value);
 
     static from(arr) {
