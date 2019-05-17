b798b5bea4c18a72e693e063e4d8048791f72854
magdalena-u <magdalena.urzon@gmail.com>
2019-05-17 09:10:06 +0200

    add comment and call the function with arguments

--- loop.js
+++ loop.js
@@ -1,7 +1,13 @@
+/* function loop that provides a for loop statment
+It takes a value, a test function, an update function, and a body
+function.*/
+
 const loop = (currentValue, test, update, body) => {
     if (test(currentValue)) {
-        body(currentValue)
-        const updateValue = update(currentValue)
-        loop(updateValue, test, update, body)
-    } else return
-}
+        body(currentValue);
+        const updateValue = update(currentValue);
+        loop(updateValue, test, update, body);
+    }
+};
+
+loop(3, n => n > 0, n => n - 1, console.log);
