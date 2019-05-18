981b37de4d649396cb1c68bffb6e19e749843e58
magdalena-u <magdalena.urzon@gmail.com>
2019-05-18 12:18:13 +0200

    add GroupIterator as separate class, change ternary for logical operator

--- 6_3_iterable.js
+++ 6_3_iterable.js
@@ -1,25 +1,48 @@
 class Group {
-    constructor(arr) {
-        this.cluster = arr;
+    constructor(array) {
+        this.array = array;
     }
-    add = value => (this.cluster.includes(value) ? null : this.cluster.push(value));
-    delete = value =>
-        this.cluster.includes(value) ? this.cluster.splice(this.cluster.indexOf(value), 1) : null;
-    has = value => this.cluster.some(item => item === value);
 
-    static from(arr) {
-        return new Group(arr);
+    static from(array) {
+        let group = new Group(array);
+        return group;
+    }
+
+    add(value) {
+        this.array.includes(value) || this.array.push(value);
+        return value;
+    }
+
+    delete(value) {
+        this.array.includes(value) && this.array.splice(this.array.indexOf(value), 1);
+        return value;
+    }
+
+    has(value) {
+        return this.array.some(item => item === value);
     }
 
     [Symbol.iterator]() {
-        let data = this.cluster
-        return {
-            next() {
-                return {
-                    done: data.length === 0,
-                    value: data.shift()
-                }
-            }
+        return new GroupIterator(this);
+    }
+}
+
+class GroupIterator {
+    constructor(cluster) {
+        (this.cluster = cluster), (this.position = 0);
+    }
+    next() {
+        if (this.position < this.cluster.array.length) {
+            let result = { done: false, value: this.cluster.array[this.position] };
+            this.position++;
+            return result;
+        } else {
+            return { done: true };
         }
     }
+}
+
+Group.from(['a', 'b', 'c']);
+for (let value of Group.from(['a', 'b', 'c'])) {
+    console.log(value);
 }
