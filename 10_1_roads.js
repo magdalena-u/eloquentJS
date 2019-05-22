9edea260dd42f4e7eee8fe1ff313615059668258
magdalena-u <magdalena.urzon@gmail.com>
2019-05-22 15:11:38 +0200

    declare places with variables,do array of arrays of strings

--- 10_1_roads.js
+++ 10_1_roads.js
@@ -1,21 +1,33 @@
 import { buildGraph } from './10_1_graph.js';
 
+const a = "Alice's House";
+const b = "Bob's House";
+const c = 'Cabin';
+const d = 'Post Office';
+const e = 'Town Hall';
+const f = "Daria's House";
+const g = "Ernie's House";
+const h = "Grete's House";
+const i = 'Farm';
+const j = 'Shop';
+const m = 'Marketplace';
+
 const roads = [
-    "Alice's House-Bob's House",
-    "Alice's House-Cabin",
-    "Alice's House-Post Office",
-    "Bob's House-Town Hall",
-    "Daria's House-Ernie's House",
-    "Daria's House-Town Hall",
-    "Ernie's House-Grete's House",
-    "Grete's House-Farm",
-    "Grete's House-Shop",
-    'Marketplace-Farm',
-    'Marketplace-Post Office',
-    'Marketplace-Shop',
-    'Marketplace-Town Hall',
-    'Shop-Town Hall',
+    [a, b],
+    [a, c],
+    [a, d],
+    [b, e],
+    [f, g],
+    [f, e],
+    [g, h],
+    [h, i],
+    [h, j],
+    [m, i],
+    [m, d],
+    [m, j],
+    [m, e],
+    [j, e],
 ];
 
-const roadGraph = buildGraph(roads.map(road => road.split('-')));
+const roadGraph = buildGraph(roads);
 console.log(roadGraph);
