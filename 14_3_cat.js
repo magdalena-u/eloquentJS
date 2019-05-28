327c475e49c92ba9537c90feadf6bcbda9f503df
magdalena-u <magdalena.urzon@gmail.com>
2019-05-28 11:04:11 +0200

    add descrive variables

--- 14_3_cat.js
+++ 14_3_cat.js
@@ -1,17 +1,23 @@
 const hat = document.getElementById('hat');
 const div = document.querySelector('div');
-const angle = Math.PI / 2;
+let angle = Math.PI / 2;
+const multiplierA = 20;
+const multiplierB = 200;
+const transformTopDiv = 50;
+const transformLeftDiv = 250;
+const transformTopHat = -30;
+const transformLeftHat = 80;
 
 function animate(time, lastTime) {
-    if (lastTime != null) {
+    if (lastTime) {
         angle += (time - lastTime) * 0.001;
     }
 
-    div.style.top = Math.sin(angle) * 20 + 50 + 'px';
-    div.style.left = Math.cos(angle) * 200 + 250 + 'px';
+    div.style.top = Math.sin(angle) * multiplierA + transformTopDiv + 'px';
+    div.style.left = Math.cos(angle) * multiplierB + transformLeftDiv + 'px';
 
-    hat.style.top = Math.cos(angle) * 10 - 30 + 'px';
-    hat.style.left = Math.sin(angle) * 20 + 80 + 'px';
+    hat.style.top = Math.cos(angle) * multiplierA - transformTopHat + 'px';
+    hat.style.left = Math.sin(angle) * multiplierA + transformLeftHat + 'px';
 
     requestAnimationFrame(newTime => animate(newTime, time));
 }
