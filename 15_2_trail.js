809ea7217c0ee55a5af425dfe864df1fb89e786c
magdalena-u <magdalena.urzon@gmail.com>
2019-06-06 11:17:27 +0200

    segregate expressions

--- 15_2_trail.js
+++ 15_2_trail.js
@@ -1,17 +1,20 @@
 const mouseMove = event => {
     const y = event.clientY;
     const x = event.clientX;
+
     draw(x, y);
 };
 
 const draw = (x, y) => {
     const star = new Element(x, y);
+
     star.init();
 };
 
 class Element {
     character = '⭐';
     nodeElement;
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -19,11 +22,14 @@ class Element {
 
     init() {
         const random = Math.floor(Math.random() * 25);
+
         this.nodeElement = document.createElement('div');
         this.nodeElement.innerHTML = this.character;
         this.nodeElement.style.left = `${this.x + random}px`;
         this.nodeElement.style.top = `${this.y + random}px`;
+
         document.body.appendChild(this.nodeElement);
+
         setTimeout(() => {
             this.nodeElement.remove();
         }, 500);
